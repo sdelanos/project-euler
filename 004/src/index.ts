bcfa06f8ce7fdad0ba5d8f44a8f40dec39174a0e
sdelanos <sebastien.delanos.perso@gmail.com>
2020-08-03 00:33:59 +0200

    problem 4 algorithm

--- 004/src/index.ts
+++ 004/src/index.ts
@@ -2,35 +2,35 @@ import Perfy from 'perfy';
 
 export default class Main {
   public static process(): number {
-    Perfy.start('perf');
     let result = 0;
-
     let product1 = 100;
     let product2 = 100;
 
-    while (product1 < 1000 && product2 < 1000) {
-      while (product2 < 999) {
-        const current: number = product1 * product2;
-        if (current.toString().length % 2 === 0) {
-          if (this.isPalindrome(current)) {
-            result = result < current ? current : result;
-          }
-        }
+    Perfy.start('perf');
+
+    // TODO: Rework algorithm to improve performance
+    while (product1 < 999 && product2 < 999) {
+      const current = product1 * product2;
+
+      if (this.isPalindrome(current)) {
+        result = result < current ? current : result;
+      }
+      product1 += 1;
+
+      if (product1 === 999) {
+        product1 = 100;
         product2 += 1;
       }
-      product2 = 100;
-      product1 += 1;
     }
-
     const perf = Perfy.end('perf');
+
     console.log(`Algorithm performance : ${perf.fullMilliseconds}ms`);
+    console.log(`Algorithm result : ${result}`);
     return result;
   }
 
   private static isPalindrome(product: number): boolean {
     const stringified = product.toString();
-    return stringified.substr(0, stringified.length / 2) === stringified.substr(stringified.length / 2).split('').reverse().join('');
+    return stringified === stringified.split('').reverse().join('');
   }
 }
-
-Main.process();
--- 004/test/index.test.ts
+++ 004/test/index.test.ts
@@ -1,5 +1,7 @@
+import Main from '../src';
+
 describe('main class tests', () => {
   it('should return the right result', () => {
-    expect(true).toBe(true);
+    expect(Main.process()).toBe(906609);
   });
 });
